841f2dd6f0ba912f2558edfd666e777a1bc9b88b
Eugenio <eugenio.ftt@gmail.com>
2020-11-17 15:09:56 +0100

    channel swap

--- Filler/app/main.c
+++ Filler/app/main.c
@@ -1,5 +1,44 @@
-#include "mylib.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+stbi_uc* swapChannels(stbi_uc* buffer, int from, int to, int channels, int buffer_size){
+
+    for (int i = 0; i < buffer_size; i++)
+    {
+        int index = i* channels;
+        char temp = buffer[index+to];
+        buffer[index+to] = buffer[index+from];
+        buffer[index+from] = temp;
+    }
+    return buffer;
+    
+}
 
 int main(){
-    hello();
+    int width;
+    int height;
+    int channels;
+    stbi_uc* data =  stbi_load("assets/red.png", &width, &height, &channels, 0);
+    if(!data){
+        puts("file non trovato");
+        return 0;
+    }
+
+    printf("Width>:%d\n", width);
+    printf("Height>:%d\n", height);
+    printf("Channels>:%d\n", channels);
+
+    printf("Pixel0 0>:%d\n", data[0]);
+    printf("Pixel0 1>:%d\n", data[1]);
+    printf("Pixel0 2>:%d\n", data[2]);
+
+
+    data  = swapChannels(data, 0, 1, channels, width*height );
+    stbi_write_png("assets/green.png",width,height,channels,data,4);
+
+    
+    stbi_image_free(data);
 }
